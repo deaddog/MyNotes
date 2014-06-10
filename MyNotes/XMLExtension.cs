0e5d098904a13b82b3ee6fa02d6762b9ac5e3e92
deaddog <mikkelslsl@gmail.com>
2014-06-10 23:03:25 +0200

    Created additional methods that internally performs typechecks, simplifying their signature. Default values are passed using boxing/unboxing.

--- MyNotes/XMLExtension.cs
+++ MyNotes/XMLExtension.cs
@@ -23,5 +23,29 @@ namespace MyNotes
                 value = defaultvalue;
             return value;
         }
+        
+
+        public static T Attribute<T>(this XElement element, XName attributename, T defaultvalue)
+        {
+            object ret = null;
+
+            if (typeof(T) == typeof(int))
+                ret = Attribute(element, attributename, (int)(object)defaultvalue, int.TryParse);
+            else
+                throw new ArgumentException("Unsupported type.");
+
+            return (T)ret;
+        }
+        public static T Element<T>(this XElement element, XName elementname, T defaultvalue)
+        {
+            object ret = null;
+
+            if (typeof(T) == typeof(int))
+                ret = Element(element, elementname, (int)(object)defaultvalue, int.TryParse);
+            else
+                throw new ArgumentException("Unsupported type.");
+
+            return (T)ret;
+        }
     }
 }
