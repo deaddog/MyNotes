8e0291c45dcc1c5785254dfc56514dbec371e7b2
deaddog <mikkelslsl@gmail.com>
2014-06-10 23:01:15 +0200

    Defined generic methods for reading the value from an attribute or element, without having to check for their existance.

--- MyNotes/XMLExtension.cs
+++ MyNotes/XMLExtension.cs
@@ -5,5 +5,23 @@ namespace MyNotes
 {
     public static class XMLExtension
     {
+        public delegate bool TryParse<T>(string input, out T value);
+
+        public static T Attribute<T>(this XElement element, XName attributename, T defaultvalue, TryParse<T> reader)
+        {
+            T value = defaultvalue;
+            var attribute = element.Attribute(attributename);
+            if (attribute != null && !reader(attribute.Value, out value))
+                value = defaultvalue;
+            return value;
+        }
+        public static T Element<T>(this XElement element, XName elementname, T defaultvalue, TryParse<T> reader)
+        {
+            T value = defaultvalue;
+            var child = element.Element(elementname);
+            if (child != null && !reader(child.Value, out value))
+                value = defaultvalue;
+            return value;
+        }
     }
 }
