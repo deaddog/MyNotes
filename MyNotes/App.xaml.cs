0ec690358ccc98cfa0b7057238857b55b5a035f9
deaddog <mikkelslsl@gmail.com>
2014-06-10 17:37:47 +0200

    The App class defines (and ensures the existance of) a path where notes can be saved.

--- MyNotes/App.xaml.cs
+++ MyNotes/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,39 @@ namespace MyNotes
     /// </summary>
     public partial class App : Application
     {
+        private static string applicationDataPath;
+        public static string ApplicationDataPath
+        {
+            get { return applicationDataPath; }
+        }
+
+        static App()
+        {
+            var roamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create);
+            roamingPath = Path.Combine(roamingPath, "DeadDog", "MyNotes");
+            ensurePath(roamingPath);
+            applicationDataPath = roamingPath;
+        }
+
+        private static void ensurePath(string path)
+        {
+            string[] levels = path.Split(Path.DirectorySeparatorChar);
+            var drive = new DriveInfo(levels[0]);
+            if (drive.DriveType == DriveType.NoRootDirectory ||
+                drive.DriveType == DriveType.Unknown)
+                throw new ArgumentException("Unable to evaluate path drive; " + levels[0], "path");
+
+            if (!drive.IsReady)
+                throw new ArgumentException("Drive '" + levels[0] + "' is not ready.", "path");
+
+            path = levels[0] + "\\";
+            for (int i = 1; i < levels.Length; i++)
+            {
+                path = Path.Combine(path, levels[i]);
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (!dir.Exists)
+                    dir.Create();
+            }
+        }
     }
 }
